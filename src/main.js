379940b413c9d86db2626cdfa51daebfc3cd599b
sisabyss <xiwei_tao@outlook.com>
2024-06-27 20:12:37 +0800

    [use naive ui, tailwind css]

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { App } from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
