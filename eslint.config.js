2b5442bb06dd966609e7165ac56e38ed4a32b678
sisabyss <xiwei_tao@outlook.com>
2024-06-27 14:54:41 +0800

    [initial commit]

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+
+export default [
+	{ files: ['**/*.{js,mjs,cjs,vue}'] },
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	pluginJs.configs.recommended,
+	...pluginVue.configs['flat/recommended'],
+	{
+		rules: {
+			// 'vue/no-used-vars': 'errors'
+		},
+	},
+];
