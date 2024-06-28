8da70c0c7160db37bd31ad8984924b7a56f2fa6b
sisabyss <xiwei_tao@outlook.com>
2024-06-28 16:02:31 +0800

    [homepage] basic

---
+++ src/components/ToVisit.vue
@@ -0,0 +1,16 @@
+<script setup>
+import PlaceCardLoader from './Loader/PlaceCardLoader.vue';
+</script>
+
+<template>
+  <div class="container mx-auto p-4">
+    <h2 class="font-semibold text-lg md:text-2xl">Place to Visit</h2>
+    <p class="text-sm text-dark mb-2">These are some places you might want to visit</p>
+
+    <div class="relative -left-[20px]">
+      <PlaceCardLoader />
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue';
-import { App } from './App.vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import App from './App.vue';
+import './style.css';
 
 const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
+app.mount('#app');
