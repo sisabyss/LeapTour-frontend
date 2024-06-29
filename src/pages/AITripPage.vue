a72cc8643cbff5778f91d2115ac39196c6126bc5
euphoriana <1563086482@qq.com>
2024-06-29 21:10:08 +0800

    new update AITrip

--- src/pages/AITripPage.vue
+++ src/pages/AITripPage.vue
@@ -47,8 +47,7 @@ const day3Places = [
                         </n-icon>
                     </div>
                     <div class="box2" style="padding: 5px;">
-                        <p style="color:darkgray;font-size: smaller;">This Trip is Powered
-                            by
+                        <p style="color:darkgray;font-size:smaller;">This Trip is Powered by
                             AI</p>
                     </div>
                 </div>
