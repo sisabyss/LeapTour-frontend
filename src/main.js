70fafe98f01f8d91ac6b39b6154b9947b31d993d
Yan.Mr <appletree2525@outlook.com>
2024-07-02 22:00:12 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,23 +6,28 @@ import './style.css';
 const app = createApp(App);
 const pinia = createPinia();
 
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory } from "vue-router";
+import { createRouter } from "vue-router";
 
 import Home from './pages/Home.vue';
 import HotelList from './pages/HotelList.vue';
 import RestaurantList from './pages/RestaurantList.vue';
 import AttractionList from './pages/AttractionList.vue';
+import TripBuild from './pages/TripBuild.vue';
+import AITripPage from './pages/AITripPage.vue';
+
 
 const routes = [
   { path: '/', component: Home },
   { path: '/hotels', component: HotelList },
   { path: '/restaurants', component: RestaurantList },
   { path: '/attractions', component: AttractionList },
-  { path: '/ai_itinerary', component: Home },
+  { path: '/ai_itinerary', component: TripBuild },
+  { path: '/aitrip', component: AITripPage },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
