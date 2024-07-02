faa146eab2b2e5df3a3180ba26822bba102df17e
sisabyss <xiwei_tao@outlook.com>
2024-07-02 17:54:20 +0800

    merge into main branch
    add AMap integration

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useMainContextStore } from './store/MainContext.js';
+const store = useMainContextStore();
+
+onMounted(() => {
+  store.init();
+});
+
 import { RouterView } from 'vue-router';
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 
 import baiduMap from 'vue3-baidu-map-gl';
@@ -37,8 +38,15 @@ const router = createRouter({
   routes,
 });
 
-app.use(baiduMap, {
-  ak: '40rN64E5W1CA7HiAYXSrxWFF2MHNa5eg',
-});
+import { createMemoryHistory, createRouter } from 'vue-router';
+
+import Home from './pages/Home.vue';
+import HotelList from './pages/HotelList.vue';
+import RestaurantList from './pages/RestaurantList.vue';
+import AttractionList from './pages/AttractionList.vue';
+import MapView from './pages/MapView.vue';
+
+
+app.use(pinia);
 app.use(router);
 app.mount('#app');
